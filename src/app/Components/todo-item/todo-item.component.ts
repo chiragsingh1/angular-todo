0e72cceb7ec05c272bfde4cdc5055503a846d820
chiragsingh1 <imchirag.2717@gmail.com>
2021-11-25 17:26:47 +0530

    finalised the app

--- src/app/Components/todo-item/todo-item.component.ts
+++ src/app/Components/todo-item/todo-item.component.ts
@@ -8,12 +8,18 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Input() i: number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+
   constructor() {}
   ngOnInit(): void {}
 
   onClick(todo: Todo) {
     this.todoDelete.emit(todo);
     console.log('onClick has been triggered!');
+  }
+  onCheckboxClick(todo: Todo) {
+    this.todoCheckbox.emit(todo);
   }
 }
--- src/app/Components/todos/todos.component.ts
+++ src/app/Components/todos/todos.component.ts
@@ -8,36 +8,31 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
+  localItem: string | null;
   constructor() {
-    this.todos = [
-      {
-        sno: 1,
-        title: 'This is title 1',
-        desc: 'description',
-        active: true,
-      },
-      {
-        sno: 2,
-        title: 'This is title 2',
-        desc: 'description2',
-        active: false,
-      },
-      {
-        sno: 1,
-        title: 'This is title 3',
-        desc: 'description3',
-        active: true,
-      },
-    ];
+    this.localItem = localStorage.getItem('todos');
+    if (this.localItem == null) {
+      this.todos = [];
+    } else {
+      this.todos = JSON.parse(this.localItem);
+    }
   }
   deleteTodo(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
   addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  toggleTodo(todo: Todo) {
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
   ngOnInit(): void {}
 }
